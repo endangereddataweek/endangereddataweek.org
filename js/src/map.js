25ff4fbf3083e1663836534bc011cb5c8af49938
Wayne Graham <wgraham@clir.org>
2017-03-05 09:48:25 -0500

    Clean up map debugging code

--- js/src/map.js
+++ js/src/map.js
@@ -32,25 +32,6 @@
         layer.bindPopup(popupContent);
     }
 
-    var events = {
-      "type": "FeatureCollection",
-      "features": [
-
-        {
-          "geometry": {
-            "type": "Point",
-            "coordinates": [42.7276362, 42.7276362]
-          },
-          "type": "Feature",
-          "properties": {
-            "popupContent": "<h2>Building a Scholarly Presence with Humanities Commons</h2><p><strong>Institution:</strong> Michigan State University</p><p><strong>Location:</strong> Linton Hall 120</p><p><strong>Date</strong>2017-04-21 at 12:30:00 PM</p><p><strong>Contact:</strong>Kristen Mapes</p>"
-          },
-          "id":  2
-        },
-
-      ]
-    };
-    
     L.geoJson([events], {
         style: function(feature) {
             return feature.properties && feature.properties.style;
