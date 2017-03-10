e99aa079da02290af814197202933342b9625f7b
hepplerj <jason.heppler@gmail.com>
2017-03-10 14:58:06 -0600

    Adding events on Github and Data Manipulation in R

--- data/events_map.js
+++ data/events_map.js
@@ -77,13 +77,49 @@ var events = {
     {
       "geometry": {
         "type": "Point",
+        "coordinates": [-96.0107049, 41.2582497]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2><a href=\"/events/2017-04-17-what-is-endangered-data?/\">What is Endangered Data?</a></h2><p><strong>Institution:</strong> University of Nebraska at Omaha</p><p><strong>Location:</strong> Criss Library, rm. 231</p><p><strong>Date:</strong> 2017-04-17 at 2:00:00 PM</p><p><strong>Contact:</strong> Jason Heppler</p>"
+      },
+      "id":  8
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-96.0107049, 41.2582497]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2><a href=\"/events/2017-04-19-an-introduction-to-github/\">An Introduction to Github</a></h2><p><strong>Institution:</strong> University of Nebraska at Omaha</p><p><strong>Location:</strong> Criss Library, rm. 231</p><p><strong>Date:</strong> 2017-04-19 at 9:00:00 AM</p><p><strong>Contact:</strong> Jason Heppler</p>"
+      },
+      "id":  9
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-96.0107049, 41.2582497]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2><a href=\"/events/2017-04-20-data-manipulation-with-r/\">Data Manipulation with R</a></h2><p><strong>Institution:</strong> University of Nebraska at Omaha</p><p><strong>Location:</strong> Criss Library, rm. 231</p><p><strong>Date:</strong> 2017-04-20 at 1:30:00 PM</p><p><strong>Contact:</strong> Jason Heppler</p>"
+      },
+      "id":  10
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
         "coordinates": [-122.331057, 47.60672]
       },
       "type": "Feature",
       "properties": {
         "popupContent": "<h2><a href=\"/events/2017-04-20-research-data-access-and-preservation-summit/\">Research Data Access and Preservation Summit</a></h2><p><strong>Institution:</strong> Seattle, WA</p><p><strong>Location:</strong> Renaissance Seattle</p><p><strong>Date:</strong> 2017-04-20 at 8:00:00 AM</p><p><strong>Contact:</strong> </p>"
       },
-      "id":  8
+      "id":  11
     },
     
     {
@@ -95,7 +131,7 @@ var events = {
       "properties": {
         "popupContent": "<h2><a href=\"/events/2017-04-21-research-data-access-and-preservation-summit/\">Research Data Access and Preservation Summit</a></h2><p><strong>Institution:</strong> Seattle, WA</p><p><strong>Location:</strong> Renaissance Seattle</p><p><strong>Date:</strong> 2017-04-21 at 8:30:00 AM</p><p><strong>Contact:</strong> </p>"
       },
-      "id":  9
+      "id":  12
     },
     
   ]
