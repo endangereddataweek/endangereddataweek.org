673b8e0b898c01b0167199368abe069f447ef4e2
Brandon <brandontlocke@gmail.com>
2017-12-03 11:51:43 -0500

    manually added lat/lon

--- data/events_map.js
+++ data/events_map.js
@@ -14,5 +14,41 @@ var events = {
       "id":  59
     },
     
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-84.4832076, 42.730871]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2 class='popup'><a href=\"/events/2018-02-28-prepare-now-to-survive-your-personal-digital-dark-age/\">Prepare Now to Survive Your Personal Digital Dark Age</a></h2><p><strong>Institution:</strong> Michigan State University</p><p><strong>Location:</strong> Digital Scholarship Lab (Main Library 2W)</p><p><strong>Date:</strong> 2018-02-28 at 2:00:00 PM</p><p><strong>Contact:</strong> Scout Calvert</p>"
+      },
+      "id":  60
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-84.4832076, 42.730871]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2 class='popup'><a href=\"/events/2018-02-27-organizing-and-using-government-data/\">Organizing and Using Government Data</a></h2><p><strong>Institution:</strong> Michigan State University</p><p><strong>Location:</strong> Digital Scholarship Lab (Main Library 2W)</p><p><strong>Date:</strong> 2018-02-27 at 2:00:00 PM</p><p><strong>Contact:</strong> Brandon Locke</p>"
+      },
+      "id":  61
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-84.4832076, 42.730871]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2 class='popup'><a href=\"/events/2018-03-02-census-data-access-importance-and-the-future/\">Census Data: Access, Importance, and the Future</a></h2><p><strong>Institution:</strong> Michigan State University</p><p><strong>Location:</strong> Digital Scholarship Lab (Main Library 2W)</p><p><strong>Date:</strong> 2018-03-02 at 3:00:00 PM</p><p><strong>Contact:</strong> Amanda Tickner</p>"
+      },
+      "id":  62
+    },
+    
   ]
 };
