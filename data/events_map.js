9dc39a07c9fa0465a8337c8e87ae52d2c4e4966d
Brandon Locke <brandontlocke@gmail.com>
2020-09-18 08:31:53 -0700

    add events

--- data/events_map.js
+++ data/events_map.js
@@ -2,5 +2,29 @@ var events = {
   "type": "FeatureCollection",
   "features": [
     
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-99.066067, 39.390897]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2 class='popup'><a href=\"/events/2020-09-21-when-data-endangers-tools-and-strategies-for-risk-assessment-policy-analysis-and-advocacy/\"><i class='fa fa-globe orange'></i> When Data Endangers: Tools and Strategies for Risk Assessment, Policy Analysis, and Advocacy</a></h2><p><strong>Institution:</strong> DLF Technologies of Surveillance Interest Group</p><p><strong>Location:</strong> DLF Technologies of Surveillance Interest Group</p><p><strong>Date:</strong> 2020-09-21 at 12:00:00 PM</p><p><strong>Contact:</strong> Scott W.H. Young & Michelle Gibeault</p>"
+      },
+      "id":  133
+    },
+    
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [145.76186, -38.11013]
+      },
+      "type": "Feature",
+      "properties": {
+        "popupContent": "<h2 class='popup'><a href=\"/events/2020-09-21-critical-data-literacy-resource-development-asyncronous-all-week/\"><i class='fa fa-globe orange'></i> Critical Data Literacy Resource Development [asyncronous, all week]</a></h2><p><strong>Institution:</strong> Endangered Data Week</p><p><strong>Location:</strong> Endangered Data Week</p><p><strong>Date:</strong> 2020-09-21 at </p><p><strong>Contact:</strong> Brandon Locke</p>"
+      },
+      "id":  134
+    },
+    
   ]
 };
